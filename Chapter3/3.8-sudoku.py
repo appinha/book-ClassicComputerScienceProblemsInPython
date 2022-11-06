0690a10b9724e8535cb4d1bbb2fec08a1f4c972d
appinha <amanda_pinha@hotmail.com>
2022-11-06 19:13:46 -0300

    Adds generator of sudoku to solve

--- Chapter3/3.8-sudoku.py
+++ Chapter3/3.8-sudoku.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 from csp import CSP, Constraint
+from random import choice, sample
 from timer import Timer
 from typing import List, Dict, Tuple, Optional
-from utils import GridLocation, display_grid
+from utils import GridLocation
 
 
 Grid = List[List[int]]
@@ -10,7 +11,7 @@ SudokuGrid = List[List[Grid]]
 SubGridLocation = Tuple[int, int]
 
 
-class SudokuGridC(SudokuGrid):
+class Sudoku(SudokuGrid):
     def __init__(self, grid: Grid) -> None:
         self.grid: Grid = grid
         self.sudoku_grid: SudokuGrid = self._build_sudoku_grid(grid)
@@ -25,6 +26,9 @@ class SudokuGridC(SudokuGrid):
                 sudoku_grid[row // 3][col // 3][row % 3][col % 3] = n
         return sudoku_grid
 
+    def _get_all_numbers(self) -> List[int]:
+        return sample(range(1, 10), 9)
+
     def _get_missing_numbers_by_grid(self) -> Dict[SubGridLocation, List[int]]:
         numbers_by_grid: Dict[SubGridLocation, List[int]] = defaultdict(list)
         for g_row, grids in enumerate(self.sudoku_grid):
@@ -36,7 +40,7 @@ class SudokuGridC(SudokuGrid):
 
         missing_nums_by_grid: Dict[SubGridLocation, List[int]] = defaultdict(list)
         for grid, numbers in numbers_by_grid.items():
-            missing_nums_by_grid[grid] = [n for n in range(1, 10) if n not in numbers]
+            missing_nums_by_grid[grid] = [n for n in self._get_all_numbers() if n not in numbers]
         return missing_nums_by_grid
 
     def _get_numbers_by_row_and_col(self) -> Dict[int, List[int]]:
@@ -50,21 +54,17 @@ class SudokuGridC(SudokuGrid):
         return numbers_by_row, numbers_by_col
 
     def get_domains(self) -> Dict[GridLocation, List[int]]:
-        numbers: List[int] = list(range(1, 10))
         possible_nums_by_grid_loc: Dict[GridLocation, List[int]] = defaultdict(list)
-        for g_row, grids in enumerate(self.sudoku_grid):
-            for g_col, grid in enumerate(grids):
-                for row, nums in enumerate(grid):
-                    for col, n in enumerate(nums):
-                        gl = GridLocation((g_row * 3) + row, (g_col * 3) + col)
-                        if n == 0:
-                            possible_nums_by_grid_loc[gl] = numbers
-                        else:
-                            possible_nums_by_grid_loc[gl] = [n]
+        for row, nums in enumerate(self.grid):
+            for col, n in enumerate(nums):
+                gl = GridLocation(row, col)
+                if n == 0:
+                    possible_nums_by_grid_loc[gl] = self._get_all_numbers()
+                else:
+                    possible_nums_by_grid_loc[gl] = [n]
         return possible_nums_by_grid_loc
 
     def get_domains_opt1(self) -> Dict[GridLocation, List[int]]:
-        all_nums = list(range(1, 10))
         missing_nums_by_grid: Dict[SubGridLocation, List[int]] = self._get_missing_numbers_by_grid()
 
         possible_nums_by_grid_loc: Dict[GridLocation, List[int]] = defaultdict(list)
@@ -74,14 +74,14 @@ class SudokuGridC(SudokuGrid):
                     for col, n in enumerate(nums):
                         gl = GridLocation((g_row * 3) + row, (g_col * 3) + col)
                         if n == 0:
-                            missing_nums = missing_nums_by_grid.get((g_row, g_col)) or all_nums
+                            missing_nums = \
+                                missing_nums_by_grid.get((g_row, g_col)) or self._get_all_numbers()
                             possible_nums_by_grid_loc[gl] = missing_nums
                         else:
                             possible_nums_by_grid_loc[gl] = [n]
         return possible_nums_by_grid_loc
 
     def get_domains_opt2(self) -> Dict[GridLocation, List[int]]:
-        all_nums = list(range(1, 10))
         missing_nums_by_grid: Dict[SubGridLocation, List[int]] = self._get_missing_numbers_by_grid()
         nums_by_row, nums_by_col  = self._get_numbers_by_row_and_col()
 
@@ -92,7 +92,8 @@ class SudokuGridC(SudokuGrid):
                     for col, n in enumerate(nums):
                         gl = GridLocation((g_row * 3) + row, (g_col * 3) + col)
                         if n == 0:
-                            missing_nums = missing_nums_by_grid.get((g_row, g_col)) or all_nums
+                            missing_nums = \
+                                missing_nums_by_grid.get((g_row, g_col)) or self._get_all_numbers()
                             possible_nums_by_grid_loc[gl] = [
                                 n
                                 for n in missing_nums
@@ -158,38 +159,46 @@ def insert_solution_into_grid(solution: Dict[GridLocation, int]) -> Grid:
     return grid
 
 
-if __name__ == '__main__':
-    timer = Timer(timer_type="performance")
-    timer.start()
-
-    grid: Grid = [
-        [0, 7, 0, 0, 1, 4, 0, 0, 0],
-        [6, 1, 2, 0, 9, 5, 3, 8, 0],
-        [3, 0, 4, 0, 6, 8, 9, 7, 1],
-        [0, 0, 0, 1, 0, 0, 0, 5, 0],
-        [2, 8, 0, 0, 0, 0, 7, 0, 3],
-        [5, 0, 3, 0, 8, 0, 0, 0, 0],
-        [0, 2, 0, 0, 3, 0, 0, 0, 0],
-        [0, 0, 6, 8, 0, 0, 1, 0, 5],
-        [0, 0, 0, 6, 7, 0, 4, 3, 0],
-    ]
-    # grid = generate_empty_grid(9)
-
-    sudoku_grid: SudokuGrid = SudokuGridC(grid)
-    print(sudoku_grid)
-
+def solve_sudoku(sudoku_grid: SudokuGrid) -> Optional[Dict[GridLocation, int]]:
     domains: Dict[GridLocation, List[int]] = sudoku_grid.get_domains_opt2()
     locations: List[GridLocation] = domains.keys()
 
     csp: CSP[GridLocation, List[int]] = CSP(locations, domains)
     csp.add_constraint(SudokuConstraint(locations))
 
-    solution: Optional[Dict[GridLocation, int]] = csp.backtracking_search()
+    return csp.backtracking_search()
+
+
+def generate_sudoku_to_solve(level: str = 'easy') -> SudokuGrid:
+    solution: Optional[Dict[GridLocation, int]] = solve_sudoku(Sudoku(generate_empty_grid(9)))
+
+    # remove some numbers from solved sudoku
+    for _ in range((9 * 9) - LEVELS[level]):
+        solution.pop(choice(list(solution.keys())))
+
+    return Sudoku(insert_solution_into_grid(solution))
+
+
+LEVELS = {
+    'easy': 37,
+    'medium': 27,
+    'hard': 17,
+}
+
+
+if __name__ == '__main__':
+    timer = Timer(timer_type="performance")
+    timer.start()
+
+    sudoku_grid: SudokuGrid = generate_sudoku_to_solve(level='medium')
+    print(sudoku_grid)
+
+    solution: Optional[Dict[GridLocation, int]] = solve_sudoku(sudoku_grid)
     if solution is None:
         print('No solution found!')
     else:
-        solved_sudoku_grid: SudokuGrid = SudokuGridC(insert_solution_into_grid(solution))
+        solved_sudoku_grid: SudokuGrid = Sudoku(insert_solution_into_grid(solution))
         print(solved_sudoku_grid)
 
     timer.stop()
-    print(f'-> elapsed time: {timer.time_sec}')
+    print(f'-> elapsed time: {timer.time_sec:.2f} seconds')
